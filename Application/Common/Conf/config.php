3f84f0125d93f7acf2b0ce5205cbcc0a35fe9ef9
wuwenbao <572300808@qq.com>
2016-09-17 13:59:14 +0800

    2016年9月17日 13:59:11

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,16 +3,16 @@
 return array(
 	/* 数据库 */
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'localhost',
+    'DB_HOST' => '139.129.212.220',
     'DB_PORT' => 3306,
-    'DB_USER' => 'root',
-    'DB_PWD' => 'root',
+    'DB_USER' => 'yingpu',
+    'DB_PWD' => 'yingpu',
     'DB_PREFIX' => 'jfsd_',
-    'DB_NAME' => 'jfsd_zhangzhilei_lantian',
+    'DB_NAME' => 'jfsd_yingpu',
 
 	/* 默认模块 */
     'DEFAULT_MODULE'    => 'Home',
-    'MODULE_ALLOW_LIST' => array('Home','Admin', 'Mobile'),
+    'MODULE_ALLOW_LIST' => array('Home','Admin', 'System'),
     'MODULE_DENY_LIST'  => array('Common','Runtime'),
 
 	/* 缓存 */
